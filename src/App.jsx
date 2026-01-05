962c60223349d1e12b9dc4c2a26c316664f8cece
bikramgiri <asgbrg.github@gmail.com>
2026-01-05 10:34:34 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home'
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} /> 
+    </Routes>
+    <Footer />
+    </BrowserRouter>
+  )
+}
+
+export default App
