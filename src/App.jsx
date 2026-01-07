851103b9d69f2538cca1f7d4e88b5ddf3e2a6b2e
bikramgiri <asgbrg.github@gmail.com>
2026-01-08 00:12:09 +0545

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
 import React from "react";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 
+// function App() {
+//   return (
+//     <HashRouter>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//       </Routes>
+//       <Footer />
+//     </HashRouter >
+//   );
+// }
+
+
+// *OR
+
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename="/portfolio">  {/* ← Add basename */}
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
       <Footer />
-    </HashRouter >
+    </BrowserRouter>
   );
 }
-
-
-// *OR
 
 export default App;
